fa9fb89d475502747aaabc1a221161c1c3e92692
zeka2021 <gerasimovajeka@gmail.com>
2021-10-10 23:19:34 +0300

    add uuid

--- src/ImageGallery/ImageGallery.js
+++ src/ImageGallery/ImageGallery.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import ImageGalleryItem from '../ImageGalleryItem';
 
 class ImageGallery extends Component {
@@ -13,7 +14,7 @@ class ImageGallery extends Component {
                     ({ id, webformatURL, largeImageURL, tags }, index) => (
                         <ImageGalleryItem
                             onModalClick={() => this.props.onModalClick(index)}
-                            key={`${id}`}
+                            key={`${id}}-${uuidv4()}`}
                             image={webformatURL}
                             modalImage={largeImageURL}
                             description={tags.split(',')[0]}
